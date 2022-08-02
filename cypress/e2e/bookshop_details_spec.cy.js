1eee1075b96a7b78f293dc904406dc0316f9e89b
Kay <kard@hotmail.co.uk>
2022-08-02 21:13:54 +0100

    ADD: shows the address of Gay's The Word

--- cypress/e2e/bookshop_details_spec.cy.js
+++ cypress/e2e/bookshop_details_spec.cy.js
@@ -4,4 +4,11 @@ describe('The bookshop information', () => {
     cy.get('.bookshop-name').first().should('contain', "Gay's the Word")
     cy.get('.bookshop-name').last().should('contain', "Round Table Books")
   })
+
+  it('shows the bookshop address',() => {
+    cy.visit('/openbook')
+    cy.get('.bookshop-address').first().should('contain', '66 Marchmont Street')
+    cy.get('.bookshop-address').first().should('contain', 'London')
+    cy.get('.bookshop-address').first().should('contain', 'WC1N 1AB')
+  })
 })
