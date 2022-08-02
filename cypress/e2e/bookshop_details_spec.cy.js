38a18539aea43cfe74a846c44a400b261220a35c
Kay <kard@hotmail.co.uk>
2022-08-02 21:17:45 +0100

    ADD: homepage shows address of Round Table Books

--- cypress/e2e/bookshop_details_spec.cy.js
+++ cypress/e2e/bookshop_details_spec.cy.js
@@ -10,5 +10,11 @@ describe('The bookshop information', () => {
     cy.get('.bookshop-address').first().should('contain', '66 Marchmont Street')
     cy.get('.bookshop-address').first().should('contain', 'London')
     cy.get('.bookshop-address').first().should('contain', 'WC1N 1AB')
+
+    cy.get('.bookshop-address').last().should('contain', '97 Granville Arcade')
+    cy.get('.bookshop-address').last().should('contain', 'Coldharbour Lane')
+    cy.get('.bookshop-address').last().should('contain', 'Brixton')
+    cy.get('.bookshop-address').last().should('contain', 'London')
+    cy.get('.bookshop-address').last().should('contain', 'SW9 8PS')
   })
 })
