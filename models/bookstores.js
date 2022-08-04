7990b0c3a575abce4e2192a0e97236185a654690
Kay <kard@hotmail.co.uk>
2022-08-04 15:15:26 +0100

    Database returns the addresses of the bookshops

--- models/bookstores.js
+++ models/bookstores.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const BookshopSchema = new mongoose.Schema({
   name: String,
-  address: String,
+  address: Array,
   postcode: String,
   longitude: String,
   latitude: String,
--- spec/models/bookstores.test.js
+++ spec/models/bookstores.test.js
@@ -5,11 +5,21 @@ const Bookshop = require("../../models/bookstores");
 
 describe('bookstores collection', () => {
   it('returns the names of the two bookshops', (done) => {
-    Bookshop.find((err, posts) => {
+    Bookshop.find((err, bookshops) => {
       expect(err).toBeNull();
-      expect(posts[0].name).toEqual("Gay's the Word")
-      expect(posts[1].name).toEqual("Round Table Books")
+      expect(bookshops[0].name).toEqual("Gay's the Word")
+      expect(bookshops[1].name).toEqual("Round Table Books")
       done();
     });
   });
+
+  it('returns the addresses of the two bookshops', (done) => {
+    Bookshop.find((err, bookshops) => {
+      expect(err).toBeNull();
+      console.log(bookshops)
+      expect(bookshops[0].address).toEqual(["66 Marchmont Street", "London", "WC1N 1AB"])
+      expect(bookshops[1].address).toEqual(["97 Granville Arcade", "Coldharbour Lane", "Brixton", "London", "SW9 8PS"])
+      done();
+    });
+  })
 })
