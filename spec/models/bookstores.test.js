c5ad6f2f3e644c8f81080ab731ae36e8eabd7174
Kay <kard@hotmail.co.uk>
2022-08-04 15:01:36 +0100

    Added MongoDB helper

---
+++ spec/models/bookstores.test.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+require("../mongodb_helper");
+const Bookstore = require("../../models/bookstores");
+
+
+describe('bookstores collection', () => {
+  it('returns the names of two bookshops', () => {
+    
+  })
+})
