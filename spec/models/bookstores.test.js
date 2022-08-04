473562121f3c736f6c88756a1d723f0670237a99
Kay <kard@hotmail.co.uk>
2022-08-04 15:10:32 +0100

    Database returns the names of both bookshops

--- spec/models/bookstores.test.js
+++ spec/models/bookstores.test.js
@@ -4,10 +4,11 @@ require("../mongodb_helper");
 const Bookshop = require("../../models/bookstores");
 
 describe('bookstores collection', () => {
-  it('returns the details of the two bookshops', (done) => {
+  it('returns the names of the two bookshops', (done) => {
     Bookshop.find((err, posts) => {
       expect(err).toBeNull();
       expect(posts[0].name).toEqual("Gay's the Word")
+      expect(posts[1].name).toEqual("Round Table Books")
       done();
     });
   });
