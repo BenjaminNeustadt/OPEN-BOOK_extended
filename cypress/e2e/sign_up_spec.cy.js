e0d17648a5e2e6d763ddf540b66dce281a3072be
Benjamin Neustadt <102234836+BenjaminNeustadt@users.noreply.github.com>
2022-08-05 17:51:11 +0200

    Modified: indentation

--- cypress/e2e/sign_up_spec.cy.js
+++ cypress/e2e/sign_up_spec.cy.js
@@ -2,8 +2,8 @@ describe('The sign up page', () => {
 	it('allows the user to sign up', () => {
 		cy.visit('/users/new');
 		cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
+		cy.get("#password").type("password");
 		cy.get('#sign-up-button').click();
 		cy.url().should("include", "/sessions/new");
-	})
+		})
 })
