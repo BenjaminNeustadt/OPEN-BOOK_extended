7177b15a2659970a1d5bd3c6c0921257a9bdebd5
Kay <kard@hotmail.co.uk>
2022-08-09 16:51:23 +0100

    Searches by tags

--- backend/controllers/bookshops.js
+++ backend/controllers/bookshops.js
@@ -19,11 +19,13 @@ const BookShopsController = {
   },
 
   SearchResults: (req, res) => {
-    Bookshop.find((err, bookshops) => {
-      const searchResult = formatter.formatName(req.query.search);
-      const bookshopIds = formatter.changeToIDS(bookshops);
-      res.render('search_results', {bookshops: bookshops, bookshopIds: bookshopIds, searchResult: searchResult})
-    })
+    if (req.query.search) {
+      Bookshop.find((err, bookshops) => {
+        const searchResult = formatter.formatName(req.query.search);
+        const bookshopIds = formatter.changeToIDS(bookshops);
+        res.render('search_results', {bookshops: bookshops, bookshopIds: bookshopIds, searchResult: searchResult})
+      })
+    } 
   }
 }
 
