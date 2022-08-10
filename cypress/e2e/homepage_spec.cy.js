dafc83ea1a3c3fa20966631584b0f203845f0b99
raphaella-rose <raphaella.rose1@gmail.com>
2022-08-10 15:48:22 +0100

    added feature test for toggle to map

--- cypress/e2e/homepage_spec.cy.js
+++ cypress/e2e/homepage_spec.cy.js
@@ -1,23 +1,29 @@
 describe('The homepage', () => {
-    it('checks that there is a title', () => {
-        cy.visit('/openbook')
-        cy.get('h1').should('contain', 'OpenBook')
-    })
-    
-    xit("should redirect to homepage when url is '/' ", () => {
-        cy.visit('/');
-        cy.url().should('eq', 'http://localhost:3030/openbook')
-    })
+  it('checks that there is a title', () => {
+      cy.visit('/openbook')
+      cy.get('h1').should('contain', 'OpenBook')
+  })
+
+  xit("should redirect to homepage when url is '/' ", () => {
+      cy.visit('/');
+      cy.url().should('eq', 'http://localhost:3030/openbook')
+  })
 
 
-    it("should have url '/' equal url '/openbook' by checking status 200", () => {
-        cy.request({
-            url: 'http://localhost:3030/',
-            failOnStatusCode:false,
-        }).then((resp) => {
-            expect(resp.status).to.eq(200)
-        })
-    })
+  it("should have url '/' equal url '/openbook' by checking status 200", () => {
+      cy.request({
+          url: 'http://localhost:3030/',
+          failOnStatusCode:false,
+      }).then((resp) => {
+          expect(resp.status).to.eq(200)
+      })
+  })
 
-
+  it("can toggle between map and list", () => {
+      cy.visit('/openbook');
+      cy.get('#toggle').click()
+      cy.get('#toggle').should('contain', 'List')
+      cy.get('#map').should('be.visible')
+      cy.get('.row').should('not.be.visible')
+  })
 })
