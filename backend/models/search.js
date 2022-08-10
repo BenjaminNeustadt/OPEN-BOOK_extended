e3fa4e2cfcd39c1d0fd6329fcb9dfd78ab9a6504
Kay <kard@hotmail.co.uk>
2022-08-10 14:39:21 +0100

    Refactored search class

--- backend/models/search.js
+++ backend/models/search.js
@@ -1,13 +1,26 @@
+const Formatter = require("../../backend/models/formatter")
+
 class Search {
   constructor(search, bookshops) {
     this.search = search;
     this.bookshops = bookshops;
+    this.formatter = new Formatter;
     return this.findSearchResults()
   }
 
   findSearchResults() {
-    const filteredBookshops = this.bookshops.filter(bookshop => bookshop.divId == this.search || bookshop.tags.includes(this.search))
+    const matchingNames = this.isSameName()
+    const matchingTags = this.includesTag()
+    const filteredBookshops = matchingNames.concat(matchingTags);
     return filteredBookshops;
+  }
+
+  isSameName() {
+    return this.bookshops.filter(bookshop => bookshop.divId == this.search)
+  }
+
+  includesTag() {
+    return this.bookshops.filter(bookshop => bookshop.tags.includes(this.search))
   }
 }
 
--- spec/models/search.test.js
+++ spec/models/search.test.js
@@ -1,6 +1,11 @@
 const Search = require("../../backend/models/search")
 
+const Formatter = require("../../backend/models/formatter")
+
 describe('Search',() => {
+  beforeEach(() => {
+    this.formatter = new Formatter()
+  })
 
   it('filters for a name',() => {
     const bookshops = [
@@ -19,6 +24,17 @@ describe('Search',() => {
       {name: "Sarah's Books", address: ["Sarah's Home", "SE4 3SD"], tags: ['miserable', 'sad'], divId: 'sarahsbooks'}
     ]
     const search = 'happy'
+    expect(new Search(search, bookshops)).toEqual([
+      {name: "Art Of Your's", address: ["53 The Street", "SE6 23S"], tags: ['happy', 'sad'], divId: 'artofyours'}
+    ]);
+  })
+
+  it('filters for an incomplete name',() => {
+    const bookshops = [
+      {name: "Art Of Your's", address: ["53 The Street", "SE6 23S"], tags: ['happy', 'sad'], divId: 'artofyours'},
+      {name: "Sarah's Books", address: ["Sarah's Home", "SE4 3SD"], tags: ['miserable', 'sad'], divId: 'sarahsbooks'}
+    ]
+    const search = 'art';
     expect(new Search(search, bookshops)).toEqual([
       {name: "Art Of Your's", address: ["53 The Street", "SE6 23S"], tags: ['happy', 'sad'], divId: 'artofyours'}
     ]);
