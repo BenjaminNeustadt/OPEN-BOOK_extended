0d5a5e07a79576f9ac4120866e8bfc03901e3977
Kay <kard@hotmail.co.uk>
2022-08-10 10:59:12 +0100

    Search form is on index page

--- cypress/e2e/search_spec.cy.js
+++ cypress/e2e/search_spec.cy.js
@@ -1,13 +1,7 @@
 describe("The search function", () => {
-  it("navigates to the search page", () => {
-    cy.visit('/openbook');
-    cy.get('.search-link').click();
-
-    cy.get('.title').should('contain', "Search")
-  })
   it("returns a bookshop based on searching name", () => {
     cy.visit('/openbook');
-    cy.get('.search-link').click();
+    
     cy.get('#search').type("Gay's the Word");
     cy.get('#submit-search').click();
 
@@ -16,7 +10,7 @@ describe("The search function", () => {
 
   it("returns a bookshop when entered in all lower case", () => {
     cy.visit('/openbook');
-    cy.get('.search-link').click();
+   
     cy.get('#search').type("round table books");
     cy.get('#submit-search').click();
 
@@ -25,7 +19,7 @@ describe("The search function", () => {
 
   it("returns a bookshop name when entered without punctuation", () => {
     cy.visit('/openbook');
-    cy.get('.search-link').click();
+
     cy.get('#search').type("gays the word");
     cy.get('#submit-search').click();
 
@@ -34,14 +28,12 @@ describe("The search function", () => {
 
   it("returns a bookshop when entered without spaces",() => {
     cy.visit('/openbook');
-    cy.get('.search-link').click();
     cy.get('#search').type("gaystheword");
     cy.get('#submit-search').click();
 
     cy.get(".bookshop-name").first().should("contain", "Gay's the Word");
 
     cy.visit('/openbook');
-    cy.get('.search-link').click();
     cy.get('#search').type("artwordsbookshop");
     cy.get('#submit-search').click();
 
@@ -50,7 +42,6 @@ describe("The search function", () => {
 
   it("searches by tags",() => {
     cy.visit('/openbook');
-    cy.get('.search-link').click();
     cy.get('#search').type("children");
     cy.get('#submit-search').click();
 
@@ -62,14 +53,12 @@ describe("The search function", () => {
 
   it("searches by tags for LGBTQ",() => {
     cy.visit('/openbook');
-    cy.get('.search-link').click();
     cy.get('#search').type("LGBTQ");
     cy.get('#submit-search').click();
 
     cy.get(".bookshop-name").first().should('exist');
 
     cy.visit('/openbook');
-    cy.get('.search-link').click();
     cy.get('#search').type("lgbtq");
     cy.get('#submit-search').click();
 
@@ -79,7 +68,6 @@ describe("The search function", () => {
 
   it("searches by tags regardless of capitalisation",() => {
     cy.visit('/openbook');
-    cy.get('.search-link').click();
     cy.get('#search').type("CHILDREN");
     cy.get('#submit-search').click();
 
