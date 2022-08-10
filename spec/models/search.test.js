888aef99813528111378170d3874470d7018f8b4
Kay <kard@hotmail.co.uk>
2022-08-10 14:59:26 +0100

    It filters using just one letter

--- spec/models/search.test.js
+++ spec/models/search.test.js
@@ -1,11 +1,6 @@
 const Search = require("../../backend/models/search")
 
-const Formatter = require("../../backend/models/formatter")
-
 describe('Search',() => {
-  beforeEach(() => {
-    this.formatter = new Formatter()
-  })
 
   it('filters for a name',() => {
     const bookshops = [
@@ -37,6 +32,19 @@ describe('Search',() => {
     const search = 'art';
     expect(new Search(search, bookshops)).toEqual([
       {name: "Art Of Your's", address: ["53 The Street", "SE6 23S"], tags: ['happy', 'sad'], divId: 'artofyours'}
+    ]);
+  })
+
+  it('filters using just one letter',() => {
+    const bookshops = [
+      {name: "Art Of Your's", address: ["53 The Street", "SE6 23S"], tags: ['happy', 'sad'], divId: 'artofyours'},
+      {name: "Sarah's Books", address: ["Sarah's Home", "SE4 3SD"], tags: ['miserable', 'sad'], divId: 'sarahsbooks'},
+      {name: "Spoon's Books", address: ["Spoon's Shed", "SE4 3SJ"], tags: ['loving', 'sweet'], divId: 'spoonsbooks'}
+    ]
+    const search = 's';
+    expect(new Search(search, bookshops)).toEqual([
+      {name: "Sarah's Books", address: ["Sarah's Home", "SE4 3SD"], tags: ['miserable', 'sad'], divId: 'sarahsbooks'},
+      {name: "Spoon's Books", address: ["Spoon's Shed", "SE4 3SJ"], tags: ['loving', 'sweet'], divId: 'spoonsbooks'}
     ]);
   })
 })
