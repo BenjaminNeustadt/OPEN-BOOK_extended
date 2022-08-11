dba93968367da58934a8842ed75d8be478b121e7
raphaella-rose <raphaella.rose1@gmail.com>
2022-08-11 08:34:20 +0100

    moved toggle function out of map.js

--- public/js/map.js
+++ public/js/map.js
@@ -120,33 +120,32 @@ return `
 getShops();
 
 
-const showMap = (searched) => {
-  
-  let buttonEl = document.querySelector('#toggle');
-  let rows = document.querySelectorAll('.shop-row');
-  let search = document.querySelector('.search-container');
 
-  if (!searched) {
-    if (buttonEl.innerHTML == "Map") {
-      document.querySelector('#map').classList.remove('hide-map')
-      map.resize()
+// const showMap = (searched) => {
   
-      rows.forEach(row => row.classList.add('hide-list'))
-      search.classList.add('hide-list')
-  
-      buttonEl.innerHTML = "List"
-    } else {
-      rows.forEach(row => row.classList.remove('hide-list'))
-      search.classList.remove('hide-list')
-  
-      document.querySelector('#map').classList.add('hide-map')
-      
-      buttonEl.innerHTML = "Map"
-    }
-  } else {
-    location.href = "/openbook";
-  }
+//   let buttonEl = document.querySelector('#toggle');
+//   let rows = document.querySelectorAll('.shop-row');
+//   let search = document.querySelector('.search-container');
  
+//   if (buttonEl.innerHTML == "Map") {
+//     document.querySelector('#map').classList.remove('hide-map')
+//     map.resize()
 
-}
+//     rows.forEach(row => row.classList.add('hide-list'))
+//     search.classList.add('hide-list')
+
+//     buttonEl.innerHTML = "List"
+//   } else if (buttonEl.innerHTML == "List") {
+//     rows.forEach(row => row.classList.remove('hide-list'))
+//     search.classList.remove('hide-list')
+
+//     document.querySelector('#map').classList.add('hide-map')
+    
+//     buttonEl.innerHTML = "Map"
+//   } else {
+//     location.href = "/openbook";
+//   }
+
+
+// }
 
