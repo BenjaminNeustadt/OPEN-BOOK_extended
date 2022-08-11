f0a01e87f6868922ac237b2fe31137459979ee01
raphaella-rose <raphaella.rose1@gmail.com>
2022-08-11 08:37:01 +0100

    extracted showMap from toggle

--- public/js/toggle.js
+++ public/js/toggle.js
@@ -1,8 +1,18 @@
+let buttonEl = document.querySelector('#toggle');
+let rows = document.querySelectorAll('.shop-row');
+let search = document.querySelector('.search-container');
+
+const showMap = () => {
+  rows.forEach(row => row.classList.remove('hide-list'))
+    search.classList.remove('hide-list')
+
+    document.querySelector('#map').classList.add('hide-map')
+    
+    buttonEl.innerHTML = "Map"
+}
+
 const toggle = () => {
-  
-  let buttonEl = document.querySelector('#toggle');
-  let rows = document.querySelectorAll('.shop-row');
-  let search = document.querySelector('.search-container');
+
  
   if (buttonEl.innerHTML == "Map") {
     document.querySelector('#map').classList.remove('hide-map')
@@ -13,12 +23,7 @@ const toggle = () => {
 
     buttonEl.innerHTML = "List"
   } else if (buttonEl.innerHTML == "List") {
-    rows.forEach(row => row.classList.remove('hide-list'))
-    search.classList.remove('hide-list')
-
-    document.querySelector('#map').classList.add('hide-map')
-    
-    buttonEl.innerHTML = "Map"
+    showMap()
   } else {
     location.href = "/openbook";
   }
