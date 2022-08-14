e86faff79f2251c61a3ee6008c45378c3beddd40
benjamin <b.james.neustadt@gmail.com>
2022-08-14 17:16:12 +0200

    Remove: commented out code in map script

--- public/js/map.js
+++ public/js/map.js
@@ -118,34 +118,3 @@ return `
 
 
 getShops();
-
-
-
-// const showMap = (searched) => {
-  
-//   let buttonEl = document.querySelector('#toggle');
-//   let rows = document.querySelectorAll('.shop-row');
-//   let search = document.querySelector('.search-container');
- 
-//   if (buttonEl.innerHTML == "Map") {
-//     document.querySelector('#map').classList.remove('hide-map')
-//     map.resize()
-
-//     rows.forEach(row => row.classList.add('hide-list'))
-//     search.classList.add('hide-list')
-
-//     buttonEl.innerHTML = "List"
-//   } else if (buttonEl.innerHTML == "List") {
-//     rows.forEach(row => row.classList.remove('hide-list'))
-//     search.classList.remove('hide-list')
-
-//     document.querySelector('#map').classList.add('hide-map')
-    
-//     buttonEl.innerHTML = "Map"
-//   } else {
-//     location.href = "/openbook";
-//   }
-
-
-// }
-
