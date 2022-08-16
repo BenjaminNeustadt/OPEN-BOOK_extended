595f2bc1ae0d7fc90be01e9bb7f76b50db14246b
benjamin <b.james.neustadt@gmail.com>
2022-08-16 23:58:33 +0200

    Fixed: whitespace in multiple files
    * put notes on how user will add a shop as a favourite inside model for
      user. This model has not yet been used.

--- backend/models/user.js
+++ backend/models/user.js
@@ -11,3 +11,10 @@ const UserSchema = new mongoose.Schema({
 const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
+
+// remove the boolean value for now
+// include only the favourited shops
+// favouritestores: Array
+// should start as an empty array, when user goes through list and clicks on favourite icon,
+// it does array push the favourite shop to it.
+//
