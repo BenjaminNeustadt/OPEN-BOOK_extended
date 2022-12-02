44e0f82492bc2fe5244a3bfb6ef45a8bc5afe9ba
benjamin <b.james.neustadt@gmail.com>
2022-12-02 18:26:24 +0100

    Persist chosen theme using local storage

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,29 @@
-const checkbox = 
-document.getElementById('checkbox');
+const checkbox = document.getElementById('checkbox');
+let darkMode = localStorage.getItem("dark-mode");
 
-checkbox.addEventListener('change', () => {
+const enableDarkMode = () => {
   //change the theme of the website
   document.body.classList.toggle('dark')
+  localStorage.setItem("dark-mode", "enabled");
+};
+
+const disableDarkMode = () => {
+  //change the theme of the website
+  document.body.classList.remove('dark')
+  localStorage.setItem("dark-mode", "disabled");
+};
+
+if (darkMode === "enabled") {
+  enableDarkMode(); // set state of darkMode on page load
+}
+
+checkbox.addEventListener('change', () => {
+  // check the localstorage for current theme
+  darkMode = localStorage.getItem("dark-mode"); // update darkMode when clicked
+  if (darkMode === "disabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
 })
+
